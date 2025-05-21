4a5ae396248cb63d525598c057a4ede44b3ca907
Lucas Duarte <lucasduarte_98@hotmail.com>
2025-05-20 23:38:10 -0300

    Project Created

---
+++ src/main/java/com/futelegends/futelist/FutelistApplication.java
@@ -0,0 +1,13 @@
+package com.futelegends.futelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutelistApplication.class, args);
+	}
+
+}
